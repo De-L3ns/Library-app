281c5cad62aec4bdba84c079dcbb831174b206bf
Laurens <70441547+De-L3ns@users.noreply.github.com>
2022-02-08 18:56:39 +0100

    adding book functionality done

--- public/script.js
+++ public/script.js
@@ -3,6 +3,7 @@
 // We will revisit this in the project at the end of this lesson. 
 // Your book objects should have the book’s title, author, 
 // the number of pages, and whether or not you have read the book
+
 let myLibrary = [];
 
 function Book(title, author, pages, isRead) {
@@ -12,28 +13,81 @@ function Book(title, author, pages, isRead) {
     this.pages = pages;
     this.isRead = isRead;
 
+    if (this.isRead) {
+
+        this.isRead = "READ";
+
+    } else {
+
+        this.isRead = "NOT READ";
+    }
+
 }
+
+let book1 = new Book("IT", "Stephen King", 400, true);
+let book2 = new Book("Harry Potter", "JK Rowling", 500, false);
+
+myLibrary.push(book1);
+myLibrary.push(book2);
+
 
 function addBookToLibrary() {
 
     let formInfo = document.getElementById("book-form");
-    let title = formInfo.elements[0].value;
-    console.log(title);
-    let author = formInfo.elements[1].value;
-    console.log(author);
-    let pages = formInfo.elements[2].value;
-    console.log(pages);
-    let isRead = formInfo.elements[3].value;
-    console.log(isRead);
 
+    // collect the values from the form
+    let title = formInfo.elements[0].value;
+    let author = formInfo.elements[1].value;
+    let pages = formInfo.elements[2].value;
+    let isRead = formInfo.elements[3].checked;
+
+    // create an object and push to the array
     let book = new Book(title, author, pages, isRead);
     myLibrary.push(book);
+    updateBookList(book);
+}
+
+let libraryBody = document.getElementById("library-body")
+
+// Show all books in the array on first load
+myLibrary.forEach(function (book) {
+
+
+    updateBookList(book);
+    
+
+})
+
+// re-usable functions
+
+function updateBookList(book) {
+
+    let bookCard = document.createElement("div");
+    bookCard.className = "book-card";
+    libraryBody.appendChild(bookCard);
+    pushTextToDiv("h2", book.title, bookCard);
+    pushTextToDiv("p", "By " + book.author, bookCard);
+    pushTextToDiv("p", "Pages: " + book.pages, bookCard);
+    pushTextToDiv("p", book.isRead, bookCard);
+}
+
+function pushTextToDiv(tag, content, div) {
+
+    const tagElement = document.createElement(tag);
+    tagElement.innerHTML = content;
+    div.appendChild(tagElement);
 
 }
 
-// testing push functionality 
-for (let i = 0; i < myLibrary.length; i++) {
+// Form code
 
-    console.log(myLibrary[i]);
+function openForm() {
 
+    document.getElementById("form-popup").style.display = "block";
+
+}
+
+function closeForm() {
+
+    document.getElementById("form-popup").style.display = "none";
 }
