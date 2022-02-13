b52ef8c9d92e117802c9bf7a9810d0aecc07fc3c
Laurens <70441547+De-L3ns@users.noreply.github.com>
2022-02-13 11:51:33 +0100

    deleting a book functionality added

--- public/script.js
+++ public/script.js
@@ -1,10 +1,7 @@
-// script to keep track of library books
-// Write a constructor for making “Book” objects. 
-// We will revisit this in the project at the end of this lesson. 
-// Your book objects should have the book’s title, author, 
-// the number of pages, and whether or not you have read the book
 
 let myLibrary = [];
+
+// book class
 
 function Book(title, author, pages, isRead) {
 
@@ -24,12 +21,7 @@ function Book(title, author, pages, isRead) {
 
 }
 
-let book1 = new Book("IT", "Stephen King", 400, true);
-let book2 = new Book("Harry Potter", "JK Rowling", 500, false);
-
-myLibrary.push(book1);
-myLibrary.push(book2);
-
+// Functions
 
 function addBookToLibrary() {
 
@@ -43,43 +35,98 @@ function addBookToLibrary() {
 
     // create an object and push to the array
     let book = new Book(title, author, pages, isRead);
+    let index = myLibrary.length;
     myLibrary.push(book);
-    updateBookList(book);
+    updateBookList(book, index);
 }
 
-let libraryBody = document.getElementById("library-body")
-
-// Show all books in the array on first load
-myLibrary.forEach(function (book) {
-
-
-    updateBookList(book);
+function deleteBookFromLibrary(index) {
     
+    let correspondingDivs = document.getElementsByClassName("book-card");
+    for (let i = 0; i < correspondingDivs.length; i++) {
+        
+        if (correspondingDivs[i].getAttribute("data-index") == index) {
 
-})
+            correspondingDivs[i].remove();
+            myLibrary.splice(index, 1);
+            // book 0, 1, 2, 3
+            // card 0, 2, 3, 4
+            // so change dataset index as from the point the book was deleted.
+            
+            for (let newIndex = i; newIndex < correspondingDivs.length; newIndex++) {
+
+                // As soon as a book is removed the corresponding div cards and buttons need to have their ID changed to match the array.
+                correspondingDivs[newIndex].dataset.index = newIndex;
+                let correspondingDeleteButton = correspondingDivs[newIndex].querySelector('.delete-button');
+                let correspondingReadButton = correspondingDivs[newIndex].querySelector('.read-button');
+                correspondingDeleteButton.setAttribute("id", newIndex);
+                correspondingReadButton.setAttribute("id", newIndex);
+                // Also changing the ID on the deleteBookFromLibrary and markRead function for future books.
+                correspondingDeleteButton.setAttribute("onclick", "deleteBookFromLibrary("+newIndex+");");
+                correspondingReadButton.setAttribute("onclick", "markAsRead("+newIndex+");");
+                
+            }
+
+        }  
+
+    }
+
+}
 
 // re-usable functions
 
-function updateBookList(book) {
+function updateBookList(book, index) {
+    
 
     let bookCard = document.createElement("div");
     bookCard.className = "book-card";
+    bookCard.dataset.index = index;
     libraryBody.appendChild(bookCard);
-    pushTextToDiv("h2", book.title, bookCard);
-    pushTextToDiv("p", "By " + book.author, bookCard);
-    pushTextToDiv("p", "Pages: " + book.pages, bookCard);
-    pushTextToDiv("p", book.isRead, bookCard);
-}
+    pushToDiv("h2", book.title, bookCard);
+    pushToDiv("p", "By " + book.author, bookCard);
+    pushToDiv("p", "Pages: " + book.pages, bookCard);
+    pushToDiv("p", book.isRead, bookCard);
 
-function pushTextToDiv(tag, content, div) {
+    // creating a button
+    
+    pushDeleteButtonToDiv("BUTTON", "Delete", bookCard, "delete-button", index);
+    pushButtonToDiv("BUTTON", "Mark Read", bookCard, "read-button", index);
+    
+    
+    
+
+
+}
+function pushToDiv(tag, content, div) {
 
     const tagElement = document.createElement(tag);
     tagElement.innerHTML = content;
     div.appendChild(tagElement);
 
 }
+function pushDeleteButtonToDiv(tag, content, div, className, index) {
 
-// Form code
+    const tagElement = document.createElement(tag);
+    tagElement.className = className
+    tagElement.innerHTML = content;
+    tagElement.setAttribute("onclick", "deleteBookFromLibrary("+index+");");
+    tagElement.setAttribute("id", index);
+    div.appendChild(tagElement);
+
+}
+
+function pushReadButtonToDiv(tag, content, div, className, index) {
+
+    const tagElement = document.createElement(tag);
+    tagElement.className = className
+    tagElement.innerHTML = content;
+    tagElement.setAttribute("onclick", "markAsRead("+index+");");
+    tagElement.setAttribute("id", index);
+    div.appendChild(tagElement);
+
+}
+
+// Form functions 
 
 function openForm() {
 
@@ -91,3 +138,30 @@ function closeForm() {
 
     document.getElementById("form-popup").style.display = "none";
 }
+
+// Show all books in the array on first load
+let libraryBody = document.getElementById("library-body");
+
+function refresh() {
+    console.log("refresh here");
+    console.log(myLibrary);
+    myLibrary.forEach(function (book, index) {
+
+
+        updateBookList(book, index);
+        
+        
+        
+    
+    })
+}
+
+refresh();
+
+
+
+
+// Button event listeners
+
+// Delete Button
+
